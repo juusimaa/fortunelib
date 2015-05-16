df38dcf26cff03112b6dd41d22c1f0d6c888c607
Jouni Uusimaa <jouni.uusimaa@gmail.com>
2015-05-16 17:37:42 +0300

    Unit test draft.

---
+++ FortuneLibTests/UtilsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FortuneLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace FortuneLib.Tests
+{
+    [TestClass()]
+    public class UtilsTests
+    {
+        [TestMethod()]
+        public void ParseFortuneTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
